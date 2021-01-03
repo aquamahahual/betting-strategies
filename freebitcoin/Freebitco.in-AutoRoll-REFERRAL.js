544ffa6360e54d3733229dfc450d7f276601b841
Daniele Veneno <76827196+aquamahahual@users.noreply.github.com>
2021-01-03 17:09:44 -0500

    Update Freebitco.in-AutoRoll-REFERRAL.js

--- freebitcoin/Freebitco.in-AutoRoll-REFERRAL.js
+++ freebitcoin/Freebitco.in-AutoRoll-REFERRAL.js
@@ -16,7 +16,7 @@ function panel_referral_init(){
 	script_output_css += ".script_referral h2 {font-size: 1.2em; color: #28731a; margin:0; }";
 	script_output_css += "</style>";
 
-	script_output += "<div class='center free_play_bonus_box_large script_referral' id='script_referral'>";
+	script_output =  "<div class='center free_play_bonus_box_large script_referral' id='script_referral'>";
 	script_output += "<h1>Multiply Betting System v."+script_version+"</h1>";
 	script_output += "<h2>Just for referrals</h2>";
 	script_output += window.G.MULTIPLY;
