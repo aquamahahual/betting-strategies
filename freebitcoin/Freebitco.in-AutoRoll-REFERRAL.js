f683156cdbb80e992cb68eb5b96ff6fc5d330f9a
Daniele Veneno <76827196+aquamahahual@users.noreply.github.com>
2021-01-07 20:51:06 -0500

    Update Freebitco.in-AutoRoll-REFERRAL.js

--- freebitcoin/Freebitco.in-AutoRoll-REFERRAL.js
+++ freebitcoin/Freebitco.in-AutoRoll-REFERRAL.js
@@ -20,13 +20,14 @@ function panel_referral_init(){
 
 	script_output_css =  "<style>";
 	script_output_css += ".cards-wrapper { display: grid; justify-content: center; align-items: center; grid-template-columns: 1fr 1fr; grid-gap: 1rem; padding: 1rem; margin: 0 auto; width: max-content; }";
-	script_output_css += ".card {height: 12em; width: 20em; font-size: 1em; color: white;border-radius: 1em ;padding: 1em; display: flex;flex-direction:column; align-items: flex-start;background-size: cover;background-position: center;box-shadow: 0 0 5em -1em black; position: relative; overflow: hidden; border: 10px solid #ccc; text-decoration: none; text-align: left;}"
+	script_output_css += ".card {height: 12em; width: 20em; font-size: 1em; border-radius: 1em ;padding: 1em; display: flex;flex-direction:column; align-items: flex-start;background-size: cover;background-position: center;box-shadow: 0 0 5em -1em black; position: relative; overflow: hidden; border: 1px solid lime; text-decoration: none; text-align: left;}"
+	script_output_css += ".colored .white {color:white; }";
 	script_output_css += ".colored .card .true {color:darkgreen; }";
 	script_output_css += ".colored .card .purple {color:purple; }";
 	script_output_css += ".colored .card .brown {color:brown; }";
 	script_output_css += ".colored .card .dimgray {color:dimgrey; }";
 	script_output_css += ".colored .card .bisque {color:bisque; }";
-	script_output_css += ".script_referral {font-size: 12px; background: #cccccc; border: 2px groove #09ff00; margin-bottom: 1em;}";
+	script_output_css += ".script_referral {font-size: 12px; background: #bbb; border: 2px groove #09ff00; margin-bottom: 1em;}";
 	script_output_css += ".script_referral h1 {font-size: 1.4em; margin: 0;}";
 	script_output_css += ".script_referral h2 {font-size: 1.2em; color: #28731a; margin:0; }";
 	script_output_css += ".script_referral.grayed {color: #a9a9a9}";
@@ -39,7 +40,7 @@ function panel_referral_init(){
 	script_output =  "<div class='center free_play_bonus_box_large script_referral colored' id='script_referral'>";
 	script_output += "<h1>Multiply Betting System v."+script_version+"</h1>";
 	script_output += "<h2>Just for referrals</h2>";
-	script_output += "<div class='cards-wrapper'>";
+	script_output += "<div class='cards-wrapper white'>";
 	
 	script_output += "<div id='card1' class='card'>";
 	script_output += "<h3>Settings</h3>";
@@ -78,13 +79,13 @@ function panel_referral_init(){
 		$("#ref_multiply_game_mode").text('Manual');
 	} else $("#ref_multiply_game_mode").text('Auto');
 	if (G_GAME_TYPE == 0) {
-		$('#ref_multiply_game_type').text('Mart Classic');	
+		$('#ref_multiply_game_type').text('Mart Classic (0)');	
 	} else if (G_GAME_TYPE == 1) {
-		$('#ref_multiply_game_type').text('Mart Min Losses');	
+		$('#ref_multiply_game_type').text('Mart Min Losses (1)');	
 	} else if (G_GAME_TYPE == 2) {
-		$('#ref_multiply_game_type').text('Mart After Losses');	
+		$('#ref_multiply_game_type').text('Mart After Losses (2)');	
 	} else if (G_GAME_TYPE == 3) {
-		$('#ref_multiply_game_type').text('Mart Min losses Incr');	
+		$('#ref_multiply_game_type').text('Mart After losses ++ (3)');	
 	}
 } 
 
