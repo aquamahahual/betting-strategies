e02bdd1d605b8a451706aa0133027fbc01747a4c
Daniele Veneno <76827196+aquamahahual@users.noreply.github.com>
2021-01-09 20:52:35 -0500

    Update Freebitco.in-AutoRoll-REFERRAL.js

--- freebitcoin/Freebitco.in-AutoRoll-REFERRAL.js
+++ freebitcoin/Freebitco.in-AutoRoll-REFERRAL.js
@@ -10,11 +10,13 @@ var script_version = 0.1;
 var max_consecutive_losts = G_getCookie('max_consecutive_losts');
 var tot_multiply_sessions = G_getCookie('tot_multiply_games');
 var tot_multiply_bets = G_getCookie('tot_multiply_bets');
+var tot_multiply_play = G_getCookie('tot_multiply_play');
 var last_multiply = Date.parse(G_getCookie("last_multiply"));
 
 if ( isNaN(parseFloat(max_consecutive_losts)) ) max_consecutive_losts = 0;
 if ( isNaN(parseFloat(tot_multiply_sessions)) ) tot_multiply_sessions = 0;
 if ( isNaN(parseFloat(tot_multiply_bets)) ) tot_multiply_bets = 0;
+if ( isNaN(parseFloat(tot_multiply_play)) ) tot_multiply_play = 0;
 if ( isNaN(parseFloat(last_multiply)) ) last_multiply = 0;
 
 
@@ -32,16 +34,18 @@ function panel_referral_init(){
 	script_output_css += ".cards-wrapper { display: grid; justify-content: center; align-items: center; grid-gap: 1rem; grid-template-columns: 1fr 1fr; padding: 0.5rem 0rem; margin: 0 auto; width: max-content; }";
 	script_output_css += ".cards-wrapper-1col { grid-template-columns: 1fr; }";
 	script_output_css += ".cards-column-wrapper { display: grid; justify-content: center; grid-template-columns: 1fr 1fr; margin: 0 auto; grid-gap: 0.5em; }";	
-	script_output_css += ".card {position: relative; height: 12em; width: 28em; font-size: 0.8em; border-radius: 1em ;padding: 3em 1em; display: flex; flex-direction: column; background-color:#666; box-shadow: 0 0 5em -1em black; border: 1px solid lime; text-decoration: none; text-align: left;}";
+	script_output_css += ".card {position: relative; height: 12em; width: 28em; font-size: 0.8em; border-radius: 1em ;padding:1.5em 1em; display: flex; flex-direction: column; background-color:#666; box-shadow: 0 0 5em -1em black; border: 1px solid; text-decoration: none; text-align: left;}";
 	script_output_css += ".card-double-size {width: 56em; }";
 	script_output_css += ".card-column {display: flex; flex-direction: column; }";
 	script_output_css += ".card-button {border: 1px solid grey; box-shadow: 0 0 5em -1em white; padding: 0.3em; background-color: beige; color: black; border-radius:10px; display: flex; flex-direction: column; text-align: center;}";
-	script_output_css += ".card-button-num {font-size: 1.4em;}";
+	script_output_css += ".card-button-num {font-size: 1.4em; line-height:1.6em;}";
 	script_output_css += ".colored .white {color:white; }";
+	script_output_css += ".colored .card {border-color: lime; }"
 	script_output_css += ".colored .card .purple {color:plum; }";
 	script_output_css += ".colored .card .coral {color:coral; }";
 	script_output_css += ".colored .card .lime {color:lime; }";
 	script_output_css += ".colored .card .lgrey {color:#bbb; }";
+
 	script_output_css += ".script_referral {font-size: 12px; background: #bbb; border: 2px groove #09ff00; margin-bottom: 1em;}";
 	script_output_css += ".script_referral h1 {font-size: 1.4em; margin: 0;}";
 	script_output_css += ".script_referral h2 {font-size: 1.2em; color: #28731a; margin:0; }";
@@ -62,41 +66,56 @@ function panel_referral_init(){
 	script_output += "<div class='cards-wrapper'>";	
 
 	script_output += "<div id='card1' class='card'>";
-	script_output += "<div id='card1' class='cards-column-wrapper'>";
-	script_output += "<h3>General Settings</h3>";
-	script_output += "<div id='card1-left' class='card-column'>";
+	script_output += "<div id='card1' class='cards-column-wrapper'>"; // card 1 column wrapper
+
+	script_output += "<div id='card1-left' class='card-column'>"; //card 1 left
 	script_output += "<span>Multiply: <span id='ref_multiply_status' class='bold'></span></span>";
 	script_output += "<span>Multiply at night: <span id='ref_multiply_at_night' class='bold'></span></span>";
 	script_output += "<span>Mode: <span id='ref_multiply_game_mode' class='bold purple'></span></span>";
 	script_output += "<span>Type: <span id='ref_multiply_game_type' class='bold purple'></span></span>";
 	script_output += "<span>Speed: <span id='ref_multiply_speed' class='bold coral'></span></span>";
+	script_output += "<span>Play Probability: <span id='ref_multiply_speed' class='bold coral'>"+G_ROLL_P+"/10</span></span>";
 	script_output += "</div>"; //card 1 left close
-	script_output += "<div id='card1-right' class='card-column'>";
-	script_output += "<div id='card1-buttons-container' class='cards-column-wrapper'>"
+
+	script_output += "<div id='card1-right' class='card-column'>"; //card 1 right
+	script_output += "<div id='card1-buttons-container' class='cards-column-wrapper'>" // button wrapper
 	script_output += "<div class='card-button'><span>Wait H</span><span class='bold coral card-button-num'>"+G_MULTIPLY_WAIT_HOURS+"</span></div>";
 	script_output += "<div class='card-button'><span>Missing H</span><span id='ref_multiply_missing_hours' class='bold coral card-button-num'>"+ref_multiply_missing_hours+"</span></div>";
 	script_output += "<div class='card-button'><span>Max Rolls </span><span class='bold coral card-button-num'>"+G_MAX_ROLLS_AT_MULTIPLY+"</span></div>";
 	script_output += "<div class='card-button'><span>Max Plays </span><span class='bold coral card-button-num'>"+G_MAX_PLAY+" </span></div>";
 	script_output += "</div>"; //card 1 right buttons close
 	script_output += "</div>"; //card 1 right close
+
 	script_output += "</div>"; //card 1 column wrapper close
 	script_output += "</div>"; //card 1 close
 
 	script_output += "<div id='card2' class='card'>";
-	script_output += "<h3>Game Type Settings</h3>";
+	script_output += "<div id='card1' class='cards-column-wrapper'>"; // card 2 column wrapper
+
+	script_output += "<div id='card2-left' class='card-column'>"; //card 2 left
+	script_output += "<div id='card2-buttons-container' class='cards-column-wrapper'>" // button wrapper
+	script_output += "<div class='card-button'><span>Wait Loss</span><span class='bold coral card-button-num'>"+G_MIN_LOSSES_BEFORE_PLAY+"</span></div>";
+	script_output += "<div class='card-button'><span>Wait Wins</span><span class='bold coral card-button-num'>"+G_WAIT_PLAY_AFTER_LOSSES+"</span></div>";
+	script_output += "<div class='card-button'><span>Odds</span><span class='bold coral card-button-num'>"+G_ODDS+"</span></div>";
+	script_output += "<div class='card-button'><span>Increase</span><span class='bold coral card-button-num'>"+G_INCR+"%</span></div>";
+	script_output += "</div>"; //card 2 left buttons close
+	script_output += "</div>"; //card 2 left close
+
+	script_output += "<div id='card2-right' class='card-column' style='text-align: right'>"; //card 2 right
 	script_output += "<span>Min Bet: <span class='bold lime'>"+parseFloat(G_MIN_BET).toFixed(8)+"</span></span>";
 	script_output += "<span>Bas Bet: <span class='bold lime'>"+parseFloat(G_BAS_BET).toFixed(8)+"</span></span>";
 	script_output += "<span>Max Bet: <span class='bold lime'>"+parseFloat(G_MAX_BET).toFixed(8)+"</span></span>";
-	script_output += "<span>Wait Before Losses: <span class='bold coral'>"+G_MIN_LOSSES_BEFORE_PLAY+"</span></span>"; 
-	script_output += "<span>Wait After Losses: <span class='bold coral'>"+G_WAIT_PLAY_AFTER_LOSSES+"</span></span>"; 
-	script_output += "<span>Odds: <span class='bold dimgray'>"+G_ODDS+"</span></span>";
-	script_output += "<span>Increase Rate: <span class='bold dimgray'>"+G_INCR+"%</span></span>";
+	script_output += "<div class='card-button' style='margin: 1em 0 0 2em;'><span>=> Accepted Consecutive Losts: </span><span id='accepted_consecutive_losts' class='bold coral card-button-num'>"+accepted_consecutive_losts+"</span></div>";
+
+	script_output += "</div>"; //card 2 right close
+
+	script_output += "</div>"; //card 2 column wrapper close
 	script_output += "</div>"; //card 2 close
 
 	script_output += "<div id='card3' class='card'>";
 	script_output += "<h3>General Stats</h3>";
 	script_output += "<span>Sessions: <span id='ref_multiply_tot_sessions'>"+tot_multiply_sessions+"</span></span>";
-	script_output += "<span>Plays: <span id='ref_multiply_tot_plays'></span></span>";
+	script_output += "<span>Plays: <span id='ref_multiply_tot_plays'></span>"+tot_multiply_play+"</span>";
 	script_output += "<span>Bets: <span id='ref_multiply_tot_bets'>"+tot_multiply_bets+"</span></span>";
 	script_output += "</div>"; //card 3 close
 	
@@ -151,15 +170,20 @@ function panel_referral_init(){
 	} else if (G_GAME_TYPE == 1) {
 		$('#ref_multiply_game_type').text('Mart Min Losses (1)');	
 	} else if (G_GAME_TYPE == 2) {
-		$('#ref_multiply_game_type').text('Mart After Losses (2)');	
+		$('#ref_multiply_game_type').text('Mart After Min '+G_MIN_LOSSES_BEFORE_PLAY+' Losses and '+G_WAIT_PLAY_AFTER_LOSSES+' win');	
 	} else if (G_GAME_TYPE == 3) {
 		$('#ref_multiply_game_type').text('Mart After losses ++ (3)');	
 	}
+
+	var accepted_consecutive_losts=0; stat_bet = G_BAS_BET;
+	while (stat_bet < G_MAX_BET) {
+		stat_bet=stat_bet+(stat_bet*90/100);
+		accepted_consecutive_losts++;
+	}
+	$('#accepted_consecutive_losts').text(accepted_consecutive_losts-1);
 } 
 
 function graphs_init () {
-	
-
 	var ctx = document.getElementById('myChart').getContext('2d');
 	var chart = new Chart(ctx, {
 	    // The type of chart we want to create
